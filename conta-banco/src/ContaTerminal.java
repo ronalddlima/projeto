053558ab74b4849e566b9a74e27bc3122af1e90e
Ronald Lima <ronald131@gmail.com>
2024-07-30 19:47:31 -0300

    atualização src

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+
+
+import javax.swing.JOptionPane;
+
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        String agencia = ("00800790");
+        String saldo =("1.200,00");
+       
+        
+     System.out.println(".......Bem-Vindo......Banco Java");
+     
+     int numero = Integer.valueOf(JOptionPane.showInputDialog("Digite Numero da sua conta"));
+     System.out.println("Por Favor, digite o numero da sua conta: " + numero);          
+     
+     
+     String nome = JOptionPane.showInputDialog("Digite seu Nome: ");
+     System.out.println("Seja muito bem-vindo " + nome);
+
+     System.out.printf("Sua agência é " + agencia 
+     + " e seu saldo é de " + saldo);
+
+
+
     }
 }
